68e8811444006d21e12ff8f79b7d74d8ee700d4b
NDucViett <vietnd01012003@gmail.com>
2024-05-09 15:46:12 +0700

    KDLSTManager

---
+++ src/test/java/com/KDLST/Manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KDLST.Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
