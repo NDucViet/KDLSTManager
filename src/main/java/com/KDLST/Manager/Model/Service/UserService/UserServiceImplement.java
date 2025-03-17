1e0bcdf31d5282615302889c3877de73a1e32523
Ngo Thao Linh <kiflinh0905@gmail.com>
2025-03-17 13:28:09 +0700

    email

--- src/main/java/com/KDLST/Manager/Model/Service/UserService/UserServiceImplement.java
+++ src/main/java/com/KDLST/Manager/Model/Service/UserService/UserServiceImplement.java
@@ -123,7 +123,7 @@ public class UserServiceImplement implements UserService {
     public void sendMail(String toEmail, String subject, String body) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom("vietndde170616@fpt.edu.vn");
+            message.setFrom("vietnguyen4002@gmail.com");
             message.setTo(toEmail);
             message.setSubject(subject);
             message.setText(body);
